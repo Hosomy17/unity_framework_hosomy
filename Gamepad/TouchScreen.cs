d0c3f89408a3551dc31388703c593b0d2e7bc2ba
hosomy17 <jackson.daniel.17dh@gmail.com>
2018-11-20 21:39:48 -0400

    Update

--- Gamepad/TouchScreen.cs
+++ Gamepad/TouchScreen.cs
@@ -13,7 +13,8 @@ public class TouchScreen : GamepadGeneric
 		inputs = new Dictionary<string, object>();
 		inputs.Add("Cursor", Vector2.zero);
 		inputs.Add("State", "Idle");
-		state = "Idle";
+        inputs.Add("Acceleration", Vector2.zero);
+        state = "Idle";
 	}
 
 	void Update ()
@@ -30,18 +31,18 @@ public class TouchScreen : GamepadGeneric
 			case "Down":
 				state = "Hold";
 				break;
-			case "Hold":
-				break;
-			case "Up":
-				break;
+			//case "Hold":
+			//	break;
+			//case "Up":
+			//	break;
 			}
 		}
 		else
 		{
 			switch(state)
 			{
-			case "Idle":
-				break;
+			//case "Idle":
+			//	break;
 			case "Down":
 				state = "Up";
 				break;
@@ -55,8 +56,14 @@ public class TouchScreen : GamepadGeneric
 		}
 		inputs ["Cursor"] = lastTouch.position;
 		inputs ["State"] = state;
+        inputs ["Acceleration"] = GetAcceleration();
 
-		controller.NewInputs(inputs);
+        controller.NewInputs(inputs);
 	}
+
+    private Vector3 GetAcceleration()
+    {
+        return Input.acceleration;
+    }
 
 }
