8f95e634aee6fab3eb78cae397c9b1e5af7b607c
Hosomy17 <jackson.daniel.17dh@gmail.com>
2019-08-08 12:07:55 -0400

    Update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# unity_framework_hosomy
-
 Framework para unity
+
+Version 2.4 - Unity 2018.3.7f1
---
+++ Tools/CustomRandom.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomRandom
+{
+    private Dictionary<string, int> indexes;
+    private int totalFactor;
+
+    public CustomRandom()
+    {
+        indexes = new Dictionary<string, int>();
+        totalFactor = 0;
+    }
+
+    //Add and edit index
+    public void NewIndex(string index, int factor)
+    {
+        if (factor < 0)
+            factor = 0;
+
+        if(!indexes.ContainsKey(index))
+        {
+            totalFactor += factor;
+            indexes.Add(index, factor);
+        }
+        else
+        {
+            totalFactor -= indexes[index];
+            totalFactor += factor;
+            indexes[index] = factor;
+        }
+    }
+
+    public float IndexChance(string index)
+    {
+        return indexes[index] / (float) totalFactor;
+    }
+
+    public int IndexFactor(string index)
+    {
+        return indexes[index];
+    }
+
+    public int GetTotalFactor()
+    {
+        return totalFactor;
+    }
+
+    public int GetTotalIndex()
+    {
+        return indexes.Count;
+    }
+
+    public string Generate()
+    {
+        if(totalFactor == 0)
+            return null;
+
+        var n = Random.Range(1, totalFactor);
+        var index = indexes.GetEnumerator();
+        do
+        {
+            index.MoveNext();
+            n -= index.Current.Value;
+        } while (n > 0);
+
+        return index.Current.Key;
+    }
+}
